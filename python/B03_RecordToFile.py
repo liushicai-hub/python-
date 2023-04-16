8111938a52353b28b707de6929845a30e34886ed
liushicai-hub <130818327+liushicai-hub@users.noreply.github.com>
2023-04-16 11:40:22 +0800

    Delete python directory
    repeat

--- python/B03_RecordToFile.py
+++
@@ -1,9 +0,0 @@
-def SaveLog(FileName,Content):
-    with open(FileName,mode='a') as f:
-        f.write(Content)
-
-
-if __name__ == '__main__':
-    Content = 'test1024'
-    SaveLog('test1024.txt',Content)
-
--- python/main.py
+++
@@ -1,3 +0,0 @@
-#嘻嘻哈哈嘿嘿吼吼呵呵咔咔恰恰hi啊hi啊嘎嘎
-print('helo qyter')
-
